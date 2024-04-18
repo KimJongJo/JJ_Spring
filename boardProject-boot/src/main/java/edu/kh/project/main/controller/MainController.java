eb62c74bdce6c8b139027f43ecd8051bf7b31f77
KimJongJo <desk1614@gmail.com>
2024-04-18 16:38:43 +0900

    board project

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.member.model.service.MemberService;
@@ -30,14 +31,28 @@ public class MainController {
 		List<String> memberList = service.selectAll();
 		
 		model.addAttribute("memberList", memberList);
-		
-		
-		
+
 		// 접두사/접미사 제외
 		return "common/main";
 	}
 
+	
+	// LoginFilter -> loginError 리다이렉트
+	// -> message 만들어서 메인 페이지로 리다이렉트
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addFlashAttribute("message", "로그인 후 이용해 주세요!");
+		
+		return "redirect:/";
+	}
 
 
+	
+	
+	
+	
+
+	
 	
 }
