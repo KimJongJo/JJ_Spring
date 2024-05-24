e08ae26388acebc03911bf333d0b40e16c16c054
giveMeTheBell <desk1614@gmail.com>
2024-05-24 11:32:48 +0900

    api

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -2,11 +2,14 @@ package edu.kh.project.main.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,13 +18,15 @@ import edu.kh.project.member.model.service.MemberService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
 @SessionAttributes({"loginMember"})
+@PropertySource("classpath:/config.properties")
 public class MainController {
+	
+	@Value("${my.public.data.service.key.decode}")
+	private String decodeServiceKey;
 	
 	private final MemberService service;
 	
@@ -46,6 +51,15 @@ public class MainController {
 		ra.addFlashAttribute("message", "로그인 후 이용해 주세요!");
 		
 		return "redirect:/";
+	}
+	
+	/* 공공데이터
+	 * 서비스키 리턴하기
+	 * */
+	@GetMapping("/getServiceKey")
+	@ResponseBody
+	public String getServiceKey() {
+		return decodeServiceKey;
 	}
 
 
